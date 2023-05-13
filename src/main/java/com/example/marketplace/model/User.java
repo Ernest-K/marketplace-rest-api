faeb4b3f4af0ae784cedfb3cd013164d223c90d9
Ernest K <ernio789@gmail.com>
2023-05-13 11:22:35 +0200

    create user model

---
+++ src/main/java/com/example/marketplace/model/User.java
@@ -0,0 +1,27 @@
+package com.example.marketplace.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+}
