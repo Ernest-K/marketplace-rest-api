add429da13c1731544f9b8b702908cf3878a126f
Ernest K <ernio789@gmail.com>
2023-05-19 15:16:59 +0200

    update user response dto

--- src/main/java/com/example/marketplace/dto/UserResponse.java
+++ src/main/java/com/example/marketplace/dto/UserResponse.java
@@ -2,5 +2,5 @@ package com.example.marketplace.dto;
 
 import java.util.List;
 
-public record UserResponse(String username, String email, List<OfferResponse> offers) {
+public record UserResponse(Long id, String username, String email, List<OfferResponse> offers) {
 }
