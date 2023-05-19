c2e2013b7f65097574be2da680e3dc3ef15abb9e
Ernest K <ernio789@gmail.com>
2023-05-19 15:59:41 +0200

    add by category id endpoint to offer controller

--- src/main/java/com/example/marketplace/controller/OfferController.java
+++ src/main/java/com/example/marketplace/controller/OfferController.java
@@ -5,10 +5,7 @@ import com.example.marketplace.service.OfferService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +28,10 @@ public class OfferController {
     @GetMapping
     public ResponseEntity<List<OfferResponse>> getAllOffers(){
         return new ResponseEntity<>(offerService.getOffers(), HttpStatus.OK);
+    }
+
+    @GetMapping(params = "categoryId")
+    public ResponseEntity<List<OfferResponse>> getOffersByCategoryId(@RequestParam Long categoryId){
+        return new ResponseEntity<>(offerService.getOffersByCategoryId(categoryId), HttpStatus.OK);
     }
 }
