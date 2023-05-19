efb639529586b2fa3d967dc5c08308a70c3d28cc
Ernest K <ernio789@gmail.com>
2023-05-19 15:18:35 +0200

    create offer dto mapper

---
+++ src/main/java/com/example/marketplace/mapper/OfferMapper.java
@@ -0,0 +1,21 @@
+package com.example.marketplace.mapper;
+
+import com.example.marketplace.dto.OfferResponse;
+import com.example.marketplace.model.Offer;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class OfferMapper implements Function<Offer, OfferResponse> {
+
+    @Override
+    public OfferResponse apply(Offer offer) {
+        return new OfferResponse(offer.getId(),
+                offer.getName(),
+                offer.getDescription(),
+                offer.getPrice(),
+                offer.getUser().getId(),
+                offer.getCategory());
+    }
+}
