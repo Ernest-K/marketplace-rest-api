4ad732be51c10aba2a0889eb0ec338d7f8d391c9
Ernest K <ernio789@gmail.com>
2023-08-15 14:38:13 +0200

    create category request dto

---
+++ src/main/java/com/example/marketplace/dto/CategoryRequest.java
@@ -0,0 +1,17 @@
+package com.example.marketplace.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryRequest {
+
+    @Size(max = 30, message = "Category name is too long (max 30 characters)")
+    @NotBlank(message = "Category name is required")
+    private String name;
+}
