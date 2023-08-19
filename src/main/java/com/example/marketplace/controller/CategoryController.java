7a3efa1501704fa9c3f8fe32916f5ce4db8c876e
Ernest K <ernio789@gmail.com>
2023-08-19 12:25:31 +0200

    add authorization to category endpoints

--- src/main/java/com/example/marketplace/controller/CategoryController.java
+++ src/main/java/com/example/marketplace/controller/CategoryController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,16 +24,19 @@ public class CategoryController {
         return new ResponseEntity<>(categoryService.getCategories(), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/categories")
     ResponseEntity<Category> createCategory(@RequestBody @Valid CategoryRequest categoryRequest){
         return new ResponseEntity<>(categoryService.createCategory(categoryRequest), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping("/categories/{id}")
     ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody @Valid CategoryRequest categoryRequest){
         return new ResponseEntity<>(categoryService.updateCategory(id, categoryRequest), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping("/categories/{id}")
     ResponseEntity<String> deleteCategory(@PathVariable Long id){
         categoryService.deleteCategory(id);
