5aa046867e5e396148e77aec4fea24e18a33b446
Ernest K <ernio789@gmail.com>
2023-09-03 15:19:37 +0200

    create offer service tests

---
+++ src/test/java/com/example/marketplace/service/OfferServiceTest.java
@@ -0,0 +1,8 @@
+package com.example.marketplace.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class OfferServiceTest {}
